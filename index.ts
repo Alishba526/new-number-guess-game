ea2a381779bc6260dff3480b81f65d7ae5bc4311
Alishba526 <159262026+Alishba526@users.noreply.github.com>
2024-04-14 18:38:22 +0500

    Add files via upload
    new number-guess-game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber=Math.floor(Math.random()*7+1 ); 
+
+const answer=await inquirer.prompt([
+   {
+       name:"userGUESSEDnumber",
+       type:"input",
+       message:"please guessed a number 1-7"
+   }
+]);
+if(answer.userGUESSEDnumber===randomNumber){
+   console.log("😎congtratulation! you winner the game🥳");  
+
+}else{
+ console.log(" 😢opps you guess wrong  number sorry you loss the game");
+ 
+}
+
+
